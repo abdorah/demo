6eaac8b5e5598db85578361392461c19320128ed
Kotbi Abderrahmane <37270435+abdorah@users.noreply.github.com>
2022-11-10 15:40:57 +0000

    use unsecure

--- src/main/java/com/example/demo/config/WebSecurityConfiguration.java
+++ src/main/java/com/example/demo/config/WebSecurityConfiguration.java
@@ -57,7 +57,6 @@ public class WebSecurityConfiguration {
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
